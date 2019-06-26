a351f19a1581e80f56510ceabf0ddb14b8db00fa
Gabriel1521 <10376496+Gabriel1521@users.noreply.github.com>
2019-06-26 12:40:17 +0800

    Create gazebo_plugin.cpp

---
+++ gazebo_plugin.cpp
@@ -0,0 +1,47 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Hello World!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
+
+$ cd /home/workspace/myrobot
+$ gedit CMakeLists.txt
+
+
+cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+
+find_package(gazebo REQUIRED)
+include_directories(${GAZEBO_INCLUDE_DIRS})
+link_directories(${GAZEBO_LIBRARY_DIRS})
+list(APPEND CMAKE_CXX_FLAGS "${GAZEBO_CXX_FLAGS}")
+
+add_library(hello SHARED script/hello.cpp)
+target_link_libraries(hello ${GAZEBO_LIBRARIES})
+
+$ cd /home/workspace/myrobot
+$ mkdir build
+$ cd build/
+$ cmake ../
+$ make # You might get errors if your system is not up to date!
+$ export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/workspace/myrobot/build
+
+
+$ cd /home/workspace/myrobot/world/
+$ gedit myworld
+
+<plugin name="hello" filename="libhello.so"/>
+
+$ cd /home/workspace/myrobot/world/
+$ gazebo myworld
